f6eee085aa85fb26097f960b590d992ece6761e9
Leander Steiner <44638905+leanderst@users.noreply.github.com>
2023-07-26 09:32:42 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace BruchrechnerLeander
+{
+    partial class Programm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Programm));
+            ErsterZaehler = new TextBox();
+            ErsterNenner = new TextBox();
+            ZweiterZaehler = new TextBox();
+            ZweiterNenner = new TextBox();
+            Rechner = new ComboBox();
+            colorDialog1 = new ColorDialog();
+            ZeahlerErgebnis = new TextBox();
+            Quadant = new TextBox();
+            NennerErgebnis = new TextBox();
+            SuspendLayout();
+            // 
+            // ErsterZaehler
+            // 
+            ErsterZaehler.Font = new Font("Segoe UI", 22F, FontStyle.Regular, GraphicsUnit.Point);
+            ErsterZaehler.Location = new Point(60, 257);
+            ErsterZaehler.Name = "ErsterZaehler";
+            ErsterZaehler.Size = new Size(200, 47);
+            ErsterZaehler.TabIndex = 1;
+            ErsterZaehler.TextAlign = HorizontalAlignment.Center;
+            ErsterZaehler.TextChanged += ErsterZaehler_TextChanged;
+            // 
+            // ErsterNenner
+            // 
+            ErsterNenner.Font = new Font("Segoe UI", 22F, FontStyle.Regular, GraphicsUnit.Point);
+            ErsterNenner.Location = new Point(60, 391);
+            ErsterNenner.Name = "ErsterNenner";
+            ErsterNenner.Size = new Size(200, 47);
+            ErsterNenner.TabIndex = 2;
+            ErsterNenner.TextAlign = HorizontalAlignment.Center;
+            ErsterNenner.TextChanged += ErsterNenner_TextChanged;
+            // 
+            // ZweiterZaehler
+            // 
+            ZweiterZaehler.Font = new Font("Segoe UI", 22F, FontStyle.Regular, GraphicsUnit.Point);
+            ZweiterZaehler.Location = new Point(545, 257);
+            ZweiterZaehler.Name = "ZweiterZaehler";
+            ZweiterZaehler.Size = new Size(200, 47);
+            ZweiterZaehler.TabIndex = 3;
+            ZweiterZaehler.TextAlign = HorizontalAlignment.Center;
+            ZweiterZaehler.Visible = false;
+            ZweiterZaehler.TextChanged += ZweiterZaehler_TextChanged;
+            // 
+            // ZweiterNenner
+            // 
+            ZweiterNenner.Font = new Font("Segoe UI", 22F, FontStyle.Regular, GraphicsUnit.Point);
+            ZweiterNenner.Location = new Point(545, 391);
+            ZweiterNenner.Name = "ZweiterNenner";
+            ZweiterNenner.Size = new Size(200, 47);
+            ZweiterNenner.TabIndex = 4;
+            ZweiterNenner.TextAlign = HorizontalAlignment.Center;
+            ZweiterNenner.Visible = false;
+            ZweiterNenner.TextChanged += ZweiterNenner_TextChanged;
+            // 
+            // Rechner
+            // 
+            Rechner.DropDownHeight = 300;
+            Rechner.DropDownStyle = ComboBoxStyle.DropDownList;
+            Rechner.DropDownWidth = 12;
+            Rechner.FlatStyle = FlatStyle.Flat;
+            Rechner.Font = new Font("Arial Narrow", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            Rechner.FormattingEnabled = true;
+            Rechner.IntegralHeight = false;
+            Rechner.Items.AddRange(new object[] { "+", "-", "×", "÷", "∧", "√" });
+            Rechner.Location = new Point(378, 327);
+            Rechner.Name = "Rechner";
+            Rechner.Size = new Size(45, 39);
+            Rechner.TabIndex = 5;
+            Rechner.SelectedIndexChanged += Rechner_SelectedIndexChanged;
+            // 
+            // ZeahlerErgebnis
+            // 
+            ZeahlerErgebnis.BackColor = SystemColors.ControlLight;
+            ZeahlerErgebnis.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            ZeahlerErgebnis.Location = new Point(290, 12);
+            ZeahlerErgebnis.Name = "ZeahlerErgebnis";
+            ZeahlerErgebnis.ReadOnly = true;
+            ZeahlerErgebnis.Size = new Size(230, 52);
+            ZeahlerErgebnis.TabIndex = 6;
+            ZeahlerErgebnis.TextAlign = HorizontalAlignment.Center;
+            ZeahlerErgebnis.Visible = false;
+            ZeahlerErgebnis.TextChanged += ZeahlerErgebnis_TextChanged;
+            // 
+            // Quadant
+            // 
+            Quadant.Font = new Font("Segoe UI", 22F, FontStyle.Regular, GraphicsUnit.Point);
+            Quadant.Location = new Point(600, 320);
+            Quadant.Name = "Quadant";
+            Quadant.Size = new Size(94, 47);
+            Quadant.TabIndex = 7;
+            Quadant.TextAlign = HorizontalAlignment.Center;
+            Quadant.Visible = false;
+            Quadant.TextChanged += Quadant_TextChanged;
+            // 
+            // NennerErgebnis
+            // 
+            NennerErgebnis.BackColor = SystemColors.ControlLight;
+            NennerErgebnis.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            NennerErgebnis.Location = new Point(290, 149);
+            NennerErgebnis.Name = "NennerErgebnis";
+            NennerErgebnis.ReadOnly = true;
+            NennerErgebnis.Size = new Size(230, 52);
+            NennerErgebnis.TabIndex = 8;
+            NennerErgebnis.TextAlign = HorizontalAlignment.Center;
+            NennerErgebnis.Visible = false;
+            NennerErgebnis.TextChanged += NennerErgebnis_TextChanged;
+            // 
+            // Programm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(NennerErgebnis);
+            Controls.Add(Quadant);
+            Controls.Add(ZeahlerErgebnis);
+            Controls.Add(Rechner);
+            Controls.Add(ZweiterNenner);
+            Controls.Add(ZweiterZaehler);
+            Controls.Add(ErsterNenner);
+            Controls.Add(ErsterZaehler);
+            Name = "Programm";
+            Load += Programm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox ErsterZaehler;
+        private TextBox ErsterNenner;
+        private TextBox ZweiterZaehler;
+        private TextBox ZweiterNenner;
+        private ComboBox Rechner;
+        private ColorDialog colorDialog1;
+        private TextBox ZeahlerErgebnis;
+        private TextBox Quadant;
+        private TextBox NennerErgebnis;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System.CodeDom.Compiler;
+using System.Diagnostics;
+
+namespace BruchrechnerLeander
+{
+    public partial class Programm : Form
+    {
+        public Programm()
+        {
+            this.Text = "GrinsenderBruchRechner";//Programm Titel wird geändert
+            InitializeComponent();
+        }
+        private void Programm_Load(object sender, EventArgs e)
+        {
+        }
+        private void ErsterZaehler_TextChanged(object sender, EventArgs e)
+        {
+            ErgebnisAnzeigeKontrolleMitQuadant();//Wenn etwas im ersten Zähler eingegeben wird wird überprüft ob alles passt und wenn alles passt wird das ergebnis ausgerechnet.
+        }
+        private void ErsterNenner_TextChanged(object sender, EventArgs e)
+        {
+            ErgebnisAnzeigeKontrolleMitQuadant();//Wenn etwas im ersten Nenner eingegeben wird wird überprüft ob alles passt und wenn alles passt wird das ergebnis ausgerechnet.
+        }
+        private void ZweiterZaehler_TextChanged(object sender, EventArgs e)
+        {
+            ErgebnisAnzeigeKontrolleOhneQuadant();//Wenn etwas im zweiten Zähler eingegeben wird wird überprüft ob alles passt und wenn alles passt wird das ergebnis ausgerechnet.
+        }
+        private void ZweiterNenner_TextChanged(object sender, EventArgs e)
+        {
+            ErgebnisAnzeigeKontrolleOhneQuadant();//Wenn etwas im zweiten Nenner eingegeben wird wird überprüft ob alles passt und wenn alles passt wird das ergebnis ausgerechnet.
+        }
+        private void Rechner_SelectedIndexChanged(object sender, EventArgs e)//Wird ausgeführt wenn der ein Rechenzeichen ausgewählt wurde
+        {
+            if (Rechner.SelectedIndex < 4) //Wenn das Rechenzeichen +, -, * oder / ist
+            {
+                ZweiterNenner.Visible = true;
+                ZweiterZaehler.Visible = true;//Der zweite Bruch wird angezeigt
+                Quadant.Visible = false;//Der Quadant wird unsichtbar gemacht
+                if (ErsterZaehler.Text != "" && ErsterNenner.Text != "" && ZweiterZaehler.Text != "" && ZweiterNenner.Text != "")//Kontrolle ob es leere Textfelder gibt
+                {
+                    ErgebnisAnzeige();//Ergebnis wird ausgerechnet
+                }
+            }
+            else//Wenn das Rechenzeichen ^ oder √ ist
+            {
+                Quadant.Visible = true;//Der Quadant wird angezeigt
+                ZweiterNenner.Visible = false;
+                ZweiterZaehler.Visible = false;//Der zweite Bruch wird Unsichtbar gemacht
+                if (ErsterZaehler.Text != "" && ErsterNenner.Text != "" && Quadant.Text != "")
+                {
+                    ErgebnisAnzeige();
+                }
+            }
+        }
+        private void Quadant_TextChanged(object sender, EventArgs e)//Wird ausgeführt wenn etwas im Quadant eingegeben wird
+        {
+            if (ErsterZaehler.Text != "" && ErsterNenner.Text != "" && Quadant.Text != "")
+            {
+                ErgebnisAnzeige();
+            }
+        }
+
+        private void ZeahlerErgebnis_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void NennerErgebnis_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void ErgebnisAnzeige()
+        {
+            if (Rechner.SelectedIndex < 4)
+            {
+                if (double.TryParse(ErsterZaehler.Text, out double z1) && double.TryParse(ErsterNenner.Text, out double n1) && double.TryParse(ZweiterNenner.Text, out double z2) && double.TryParse(ZweiterZaehler.Text, out double n2))//Kontrolle ob andere Textzeichen als Zahlen im den beiden Brüchen stehen.
+                {
+                    Ergebnis();//Das ergebnis wird ausgerechnet und angezeigt
+                }
+            }
+            else
+            {
+                if (double.TryParse(ErsterZaehler.Text, out double z12) && double.TryParse(ErsterNenner.Text, out double n12) && double.TryParse(Quadant.Text, out double z22))//Kontrolle ob andere Textzeichen als Zahlen im ersten Bruch und dem Quadant steht
+                {
+                    Ergebnis();
+                }
+            }
+        }
+        private void Ergebnis()
+        {
+            double kgV;
+            switch (Rechner.SelectedIndex)//Das Ergebnis wird jeh nach Wahl des Rechenzeichen ausgerechnet
+            {
+                case 0:// +
+                    kgV = KGV(Convert.ToDouble(ErsterNenner.Text), Convert.ToDouble(ZweiterNenner.Text));//kgV wird ausgerechnet
+                    ZeahlerErgebnis.Text = Convert.ToString((Convert.ToDouble(ErsterZaehler.Text) * (kgV / Convert.ToDouble(ErsterNenner.Text)) + (Convert.ToDouble(ZweiterZaehler.Text) * (kgV / Convert.ToDouble(ZweiterNenner.Text)))));//Der Zähler wird ausgerechnet
+                    NennerErgebnis.Text = Convert.ToString(kgV);//Der Nenner wird ausgerechnet
+                    break;
+                case 1:// -
+                    kgV = KGV(Convert.ToDouble(ErsterNenner.Text), Convert.ToDouble(ZweiterNenner.Text));
+                    ZeahlerErgebnis.Text = Convert.ToString((Convert.ToDouble(ErsterZaehler.Text) * (kgV / Convert.ToDouble(ErsterNenner.Text)) - (Convert.ToDouble(ZweiterZaehler.Text) * (kgV / Convert.ToDouble(ZweiterNenner.Text)))));
+                    NennerErgebnis.Text = Convert.ToString(kgV);
+                    break;
+                case 2:// *
+                    ZeahlerErgebnis.Text = Convert.ToString(Convert.ToDouble(ErsterZaehler.Text) * Convert.ToDouble(ZweiterZaehler.Text));
+                    NennerErgebnis.Text = Convert.ToString(Convert.ToDouble(ErsterNenner.Text) * Convert.ToDouble(ZweiterNenner.Text));
+                    break;
+                case 3:// /
+                    ZeahlerErgebnis.Text = Convert.ToString(Convert.ToDouble(ErsterZaehler.Text) * Convert.ToDouble(ZweiterNenner.Text));
+                    NennerErgebnis.Text = Convert.ToString(Convert.ToDouble(ErsterNenner.Text) * Convert.ToDouble(ZweiterZaehler.Text));
+                    break;
+                case 4:// ^
+                    ZeahlerErgebnis.Text = Convert.ToString(Math.Pow(Convert.ToDouble(ErsterZaehler.Text), Convert.ToDouble(Quadant.Text)));
+                    NennerErgebnis.Text = Convert.ToString(Math.Pow(Convert.ToDouble(ErsterNenner.Text), Convert.ToDouble(Quadant.Text)));
+                    break;
+                case 5:// √
+                    ZeahlerErgebnis.Text = Convert.ToString(Math.Pow(Convert.ToDouble(ErsterZaehler.Text), 1 / Convert.ToDouble(Quadant.Text)));
+                    NennerErgebnis.Text = Convert.ToString(Math.Pow(Convert.ToDouble(ErsterNenner.Text), 1 / Convert.ToDouble(Quadant.Text)));
+                    break;
+            }
+            double ggt = GGT(Convert.ToDouble(ZeahlerErgebnis.Text), Convert.ToDouble(NennerErgebnis.Text));//GGT wird ausgerechnet
+            double temp = Convert.ToDouble(ZeahlerErgebnis.Text);//Der Inhalt vom Zähler des Ergebnis wird in temp gespeichert
+            ZeahlerErgebnis.Text = Convert.ToString(temp /= ggt);//Der Zähler vom Ergebnis wird gekürzt
+            temp = Convert.ToDouble(NennerErgebnis.Text);//Der Inhalt vom Nenner des Ergebnis wird in temp gespeichert
+            NennerErgebnis.Text = Convert.ToString(temp /= ggt);//Der Nenner vom Ergebnis wird gekürzt
+            ZeahlerErgebnis.Visible = true;
+            NennerErgebnis.Visible = true;//Der Ergebnisbruch wird angezeigt
+        }
+        private double KGV(double Zahl1, double Zahl2)//KGV wird ausgerechnet
+        {
+            Zahl1 = (UInt64)Zahl1;
+            Zahl2 = (UInt64)Zahl2;
+            double MAX = Math.Max(Zahl1, Zahl2);
+            double MIN = Math.Min(Zahl1, Zahl2);
+            double KGV = MAX;
+            while (KGV % MIN != 0)
+            {
+                KGV += MAX;
+            }
+            return KGV;
+        }
+        private double GGT(double Zahl1, double Zahl2)//GGT wird ausgerechnet
+        {
+            Zahl1 = (UInt64)Zahl1;
+            Zahl2 = (UInt64)Zahl2;
+            while (Zahl2 != 0)
+            {
+                double temp = Zahl2;
+                Zahl2 = Zahl1 % Zahl2;
+                Zahl1 = temp;
+            }
+            return Zahl1;
+        }
+        private void ErgebnisAnzeigeKontrolleMitQuadant()//Kontrolle befohr der Ausrechnung
+        {
+            if (Rechner.TabIndex < 4)
+            {
+                if (ErsterZaehler.Text != "" && ErsterNenner.Text != "" && ZweiterZaehler.Text != "" && ZweiterNenner.Text != "")
+                {
+                    ErgebnisAnzeige();
+                }
+            }
+            else
+            {
+                if (ErsterZaehler.Text != "" && ErsterNenner.Text != "" && Quadant.Text != "")
+                {
+                    ErgebnisAnzeige();
+                }
+            }
+        }
+        private void ErgebnisAnzeigeKontrolleOhneQuadant()//Kontrolle befohr der Ausrechnung
+        {
+            if (ErsterZaehler.Text != "" && ErsterNenner.Text != "" && ZweiterZaehler.Text != "" && ZweiterNenner.Text != "")
+            {
+                ErgebnisAnzeige();
+            }
+        }
+    }
+}
